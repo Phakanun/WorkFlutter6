6e3b195abf8023972e47fe2839b027530d81595f
Phakanun <s6504062630201@email.kmutnb.ac.th>
2025-01-27 10:38:39 +0700

    update

--- lib/myhome_page.dart
+++ lib/myhome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app2/welcome_page.dart';
 
 class MyhomePage extends StatefulWidget {
   const MyhomePage({super.key});
@@ -8,37 +9,58 @@ class MyhomePage extends StatefulWidget {
 }
 
 class _MyhomePageState extends State<MyhomePage> {
+  /*final List<String> products = <String>[
+    'AAA',
+    'BBB',
+    'CCC',
+    'DDD',
+    'EEE'
+  ];*/
+  Future<String> fetchData() async{
+    await Future.delayed(const Duration(seconds: 3));
+    return "Hello, FutureBuilder";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Slide'),
       ),
-      body: ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('OwO', style: TextStyle(fontSize: 20),),
-            subtitle: Text('Hello'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            tileColor: Colors.white60,
-          ),
-          ListTile(
-            leading: Icon(Icons.person_2),
-            title: Text('OwO', style: TextStyle(fontSize: 20),),
-            subtitle: Text('Hello'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            tileColor: Colors.white60,
-          ),
-          ListTile(
-            leading: Icon(Icons.person_3),
-            title: Text('OwO', style: TextStyle(fontSize: 20),),
-            subtitle: Text('Hello'),
-            trailing: Icon(Icons.arrow_forward_ios),
-            tileColor: Colors.white60,
-          ),
-        ],
+      body: Center(
+        child: FutureBuilder(
+            future: fetchData(),
+            builder: (context,snapshot){
+              if (snapshot.hasData) {
+                return Text('Result: ${snapshot.data}',
+                    style: TextStyle(fontSize: 20));
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            }),
       )
+      /*body: ListView.separated(
+          itemCount: products.length,
+          itemBuilder: (context,index) => ListTile(
+              title: Text(
+                products[index],
+                style: TextStyle(fontSize: 20),
+              )
+          ),
+          separatorBuilder: (context, int index) => const Divider()
+      ),*/
+      /*ListTile(
+        leading: Icon(Icons.person),
+        title: Text('OwO', style: TextStyle(fontSize: 20),),
+        subtitle: Text('Hello'),
+        trailing: Icon(Icons.arrow_forward_ios),
+        tileColor: Color.fromARGB(60, 127, 62, 152),
+        onTap: ()=> Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => WelcomePage()),
+        ),
+      ),*/
     );
   }
 }
